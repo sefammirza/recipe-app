1dd24dd1a70e94f00fcfc4648c33739e033bc579
sefammirza <sefammirza@gmail.com>
2022-06-25 22:41:54 +0200

    handleview

--- src/App.js
+++ src/App.js
@@ -29,6 +29,17 @@ function App() {
     })
   }, [])
 
+  const handleView = id => {
+    const recipesClone = [...recipes]
+    recipesClone.forEach(recipe => {
+    if (recipe.id === id){
+      recipe.viewing = !recipe.viewing
+    }else {
+      recipe.viewing = false
+    }})
+    setRecipes(recipesClone)
+  }
+
   return (
     <div className="App">
      <h1>Recipe App</h1>
@@ -40,6 +51,26 @@ function App() {
         <div className="recipe" key={recipe.id}>
           <h3>{recipe.title}</h3>
           <p dangerouslySetInnerHTML={{ __html:recipe.desc}}></p>
+         {recipe.viewing &&  <div>
+        <h4>Ingredients</h4>
+        <ul>
+
+          {recipe.ingredients.map((ingredient, i) => (
+            <li key={i}>{ingredient}</li>
+          ))}
+        </ul>
+        <h4>Steps</h4>
+        <ol>
+          {recipe.steps.map((step, i) => (
+            <li key={i}>{step}</li>
+          ))}
+        </ol>
+        </div>}
+
+        <div className="buttons">
+          <button onClick={() => handleView(recipe.id)}>View {recipe.viewing ? 'less' : 'more'}</button>
+          <button className="remove">Remove</button>
+        </div>
         </div>
       ))}
 
